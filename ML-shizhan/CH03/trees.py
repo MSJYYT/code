cefeca12a9f9e1e56c0625782dbbdf69a264adfb
MSJYYT <641833629@qq.com>
2018-04-17 22:20:49 +0800

    更新

--- ML-shizhan/CH03/trees.py
+++ ML-shizhan/CH03/trees.py
@@ -1,15 +1,16 @@
 from math import log
+import operator
 
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
     for featVec in dataSet:#遍历每个实例，统计标签的频次
-        currentLabel = featVec[-1]
+        currentLabel = featVec[-1]    #取得最后一列数据，计算该属性取值情况有多少个
         if currentLabel not in labelCounts.keys():labelCounts[currentLabel] = 0
         labelCounts[currentLabel] += 1
     shannonEnt = 0.0
     for key in labelCounts:
-        prob = float(labelCounts[key])/numEntries#标签的概率分布
+        prob = float(labelCounts[key])/numEntries   #标签的概率分布
         shannonEnt -= prob * log(prob,2)
     return shannonEnt
 
@@ -48,3 +49,31 @@ def chooseBestFeatureToSplit(dataSet):
             bestInfoGain = infoGain
             bestFeature = i
     return bestFeature
+
+def majorityCnt(clasList):
+    classCount = {}
+    for vote in clasList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(),
+                              key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0=]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet
+                                                  (dataSet,bestFeat,value),subLabels)
+    return myTree
+

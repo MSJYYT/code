d95bb1cac66d35d3867a85e52f608ad4502f33e8
MSJYYT <641833629@qq.com>
2018-04-21 15:35:06 +0800

    更新4.21公司

---
+++ ML-shizhan/CH05/logRegres.py
@@ -0,0 +1,11 @@
+def loadDataSet():
+    dataMat = [];labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat,labelMat
+
+def sigmoid(inX):
+    return 1.0/(1+exp(-inX))

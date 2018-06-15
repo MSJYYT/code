38c7303916feb36e4c80a73ae72c87d7ccdb59a2
MSJYYT <641833629@qq.com>
2018-06-15 17:03:42 +0800

    6.15公司

--- TensorFlow-shizhan/CH04/Masking Noise.py
+++ TensorFlow-shizhan/CH04/Masking Noise.py
@@ -37,8 +37,10 @@ class AdditiveGaussianNoiseAutoencoder(object):
         self.optimizer = optimizer.minimize(self.cost)
 
         init = tf.global_variables_initializer()
-        self.sess = tf.Session
+        self.saver = tf.train.Saver()
+        self.sess = tf.Session()
         self.sess.run(init)
+
     def _initialize_weights(self):
         all_weights = dict()
         all_weights['w1'] = tf.Variable(xavier_init(self.n_input,
@@ -78,3 +80,32 @@ def standard_scale(X_train,X_test):
     X_train = preprocessor.transform(X_train)
     X_test = preprocessor.transform(X_test)
     return X_train,X_test
+def get_random_block_from_data(data,batch_size):
+    start_index = np.random.randint(0,len(data) - batch_size)
+    return data[start_index:(start_index+ batch_size)]
+X_train,X_test = standard_scale(mnist.train.images,mnist.test.images)
+n_samples = int(mnist.train.num_examples)
+training_epochs = 20
+batch_size = 128
+display_step = 1
+
+autoencoder = AdditiveGaussianNoiseAutoencoder(n_input=784,
+                                               n_hidden=200,
+                                               transfer_function=tf.nn.softplus,
+                                               optimizer=tf.train.AdamOptimizer(learning_rate=0.001),
+                                               scale=0.01)
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(n_samples / batch_size)
+    for i in range(total_batch):
+        batch_xs = get_random_block_from_data(X_train,batch_size)
+        cost = autoencoder.partial_fit(batch_xs)
+        avg_cost += cost / n_samples * batch_size
+
+    if epoch % display_step == 0:
+        print('Epoch:','%04d' % (epoch + 1),'cost=','{:.9f}'.format(avg_cost))
+
+print('total cost:'+str(autoencoder.calc_total_cost(X_test)))
+autoencoder.saver.save(autoencoder.sess,'model/model.ckpt')
+autoencoder.sess.close()
